2881f098e26981bf6e21788f80b51c0dd7488da1
Nicholas Otero <notero@red-shift.tech>
2020-07-21 00:50:49 -0400

    Added header of dijkstra function.

--- include/rift/algorithms/search/dijkstra.hpp
+++ include/rift/algorithms/search/dijkstra.hpp
@@ -32,6 +32,7 @@
 
 #pragma once
 
+#include <algorithm>
 #include <concepts>
 #include <limits>
 #include <optional>
@@ -42,11 +43,8 @@
 #include <rift/util/rustify.hpp>
 
 namespace rift {
-    template <std::equality_comparable T>
-    struct DijkstraState {
-        std::vector<T> openSet;
-    };
-    
+namespace detail {
+
     template <std::equality_comparable T, typename C>
         requires std::is_arithmetic_v<C>
     struct DijkstraNode {
@@ -54,32 +52,71 @@ namespace rift {
          * Constructor
          */
         DijkstraNode(T dataIn) : data(dataIn) {}
-        
+
         /** The data represented by this node */
         T data;
-        
+
         /** The parent of this node */
         T* parent;
-        
+
         /** The accumulated cost to come to this node*/
         C costToCome = std::numeric_limits<C>::max();
-        
+
         /** Flag indicating if this node is on the open set */
         bool isOpen = false;
-        
+
         /** Flag indicating if this node has been removed from the open set */
         bool isClosed = false;
     };
-    
+
+    template <std::equality_comparable T, typename C>
+    struct DijkstraState {
+        std::vector<DijkstraNode<T, C>> openSet;
+    };
+}
+        
     template <std::equality_comparable T, NeighborFn<T> N, CostFn<T> G>
     fn dijkstra(T start, T goal, N neighborFn, G costFn) 
         -> std::optional<std::vector<T>> {
         
         using Cost = std::invoke_result_t<G, T, T>;
-        using Node = DijkstraNode<T, Cost>;
-        std::vector<T> openSet, result;
+        using State = detail::DijkstraState<T, Cost>;
+        using Node = detail::DijkstraNode<T, Cost>;
+        std::vector<T> result;
+        State searchState;
+        let sortNodes = [](let l, let r) {return l.costToCome > r.costToCome;};
+        let push = [&openSet = searchState.openSet, &sortNodes](Node& n) {
+            if (n.isClosed) return;
+            if (n.isOpen) {
+                auto it = std::ranges::find_if(
+                    openSet, [&n](let f) { return n.data == f.data; });
+                if (it == end(openSet)) {
+                    goto pushNew;
+                } else {
+                    it->costToCome = n.costToCome;
+                    std::make_heap(begin(openSet), end(openSet), sortNodes);
+                }
+            } else {
+                pushNew:
+                n.isOpen = true;
+                openSet.push_back(n);
+                std::push_heap(begin(openSet), end(openSet), sortNodes);
+            }
+        };
+        let pop = [&openSet = searchState.openSet, &sortNodes] {
+            std::pop_heap(begin(openSet), end(openSet), sortNodes);
+            auto&& node = std::move(openSet.back());
+            openSet.pop_back();
+            node.isClosed = true;
+            return node;
+        };
+        
         Node startNode(start), goalNode(goal);
         startNode.costToCome = 0;
+        searchState.openSet.push_back(startNode);
+        while (!searchState.openSet.empty()) {
+            let currentNode = pop();
+        }
         result.push_back(start);
         result.push_back(goal);
         return result;
